37050dc9ec55661efdb07a26035ff5a49e6598de
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2024-10-17 17:19:57 +0400

    error

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
 import './App.css'
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {ApiTodoLists} from "./api/api-todolists";
 import {TodoList} from "./components/todolist/todoList";
+import {v1} from "uuid";
+import {TodoLists} from "./types/types.ts";
 
 function App() {
+    const [todos, setTodos] = useState<TodoLists>({
+        id: v1(),
+        title: 'What to learn',
+        addedDate: '14.08.1976',
+        order: 14
+    });
 
     useEffect(() => {
-        ApiTodoLists.getTodoLists().then( (res)=>{
+        ApiTodoLists.getTodoLists().then((res) => {
             console.log(res.data)
-        } )
+        })
     }, []);
+
+    const changeTitle = (title: string) => {
+        setTodos({...todos, title})
+    }
 
     return (
         <>
-            <TodoList/>
+            <TodoList
+                todos={todos}
+                changeTitle={changeTitle}
+            />
         </>
     )
 }
--- src/components/todolist/todoList.tsx
+++ src/components/todolist/todoList.tsx
@@ -1,14 +1,40 @@
 import {useState} from "react";
+import {TodoLists} from "../../types/types.ts";
 
-export const TodoList = () => {
-    const [title, setTitle] = useState('')
+type TodoListProps = {
+    todos: TodoLists
+    changeTitle: (title: string)=>void
+}
+
+export const TodoList = (props: TodoListProps) => {
+    const {changeTitle, todos} = props
+
+    const [item, setItem] = useState('')
+    const [error, setError] = useState<null|string>(null)
+
+
+    const changeItemHandler = () => {
+        if (item.trim() !== ''){
+            changeTitle(item.trim())
+            setItem('')
+        }else {
+            setError('Title is required')
+        }
+    }
 
     return (
         <div>
+            <div>
+                {todos.title}
+            </div>
+            <button onClick={changeItemHandler}>+</button>
             <input
-                value={title}
-                onChange={ (e)=>{setTitle(e.currentTarget.value)} }
+                value={item}
+                onChange={(e) => {
+                    setItem(e.currentTarget.value)
+                }}
             />
+            {error && <div>{error}</div> }
         </div>
     );
 };
