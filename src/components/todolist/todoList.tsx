9ce62f260c93aa55989e2e2b5497d6f45a992e75
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2024-11-26 23:27:22 +0400

    select

--- src/components/todolist/todoList.tsx
+++ src/components/todolist/todoList.tsx
@@ -1,8 +1,15 @@
+import * as SelectRadix from "@radix-ui/react-select";
+
 export const TodoList = () => {
 
     return (
         <div>
             Hello World
+            <SelectRadix.Root
+                name={'Hello'}
+            >
+
+            </SelectRadix.Root>
         </div>
     );
 };
