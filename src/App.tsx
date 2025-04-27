546515178c859f16de82a7d857bdc24208eed73c
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2025-04-27 18:31:24 +0400

    dark and light theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css'
+import { ThemeProvider } from "./components/theme-provider/theme-provider"
+import {ModeToggle} from "./components/mode-toggle/mode-toggle.tsx";
 
 function App() {
-
-  return (
-    <div>
-    </div>
-  )
+    return (
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <ModeToggle/>
+        </ThemeProvider>
+    )
 }
 
 export default App
